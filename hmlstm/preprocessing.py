ee3fb4be2c7ea7282bd3a111897cde940568d743
valentin <stauber.valentin@gmail.com>
2017-08-20 22:42:45 +0100

    Added documentation

--- hmlstm/preprocessing.py
+++ hmlstm/preprocessing.py
@@ -16,7 +16,7 @@ def load_text(text_path, truncate_len, step_size, batch_size, num_chars):
     with open(text_path, 'r') as f:
         text = f.read(num_chars)
         text = text.replace('\n', ' ')
-        text = re.sub(' +', ' ', text).lower()
+        text = re.sub(' +', ' ', text).lower()  # removes +?
 
     signals = []
     start = 0
@@ -40,7 +40,7 @@ def one_hot_encode(text):
 
     for i, t in enumerate(text):
         out[i, get_index(t)] = 1
-
+    # out = text
     return out
 
 
@@ -62,12 +62,33 @@ def prepare_inputs(batch_size=10,
                    text_path='text8.txt',
                    step_size=None,
                    num_batches=None):
+    """
+    :param batch_size: # of text segments of length truncate_len per batch
+    :param truncate_len: length of input sequence for RNN (how many input neurons involved)
+    :param text_path: path where to load the text from
+    :param step_size: # of chars to advance for the next segment
+    :param num_batches: if not given, then ceil(num_segments / batch_size), if given, only batch_size * num_batches * truncate_len of he text is loaded
+    :return: batches_in: contains the input data, each batch contains truncate_len chars, with a relative shift of step_size
 
+    First the a number of segments of length truncate_len and stride step_size are created.
+    There should be roughly (text_length - truncate_len)/step_size of them
+    These segments are then collected into batches of batch_size
+
+    A batch is a collection of training samples that are used together to update the network weights.
+    If the batch contains ALL training samples, then we have batch grad descent (expensive and doesn't
+    generalize well)
+    Usually batches are of small size ~10, this is mini-batch GD. If we use one sample at a time
+    (batch_size=1) this is what ppl usually call stochastic GD (SGD).
+
+    So our segmentation is: we generate n_samples training examples of length truncate_len.
+    We then group these into n_batches batches of size batch_size
+    (so ideally, num_batches * batch_size = n_samples, assuming divisibility)
+    """
     if step_size is None:
-        step_size = truncate_len // 2
+        step_size = truncate_len // 2  # seems like a pretty random default...
 
     if num_batches is None:
-        y = text(text_path, truncate_len, step_size, batch_size)
+        y = text(text_path, truncate_len, step_size, batch_size)  # here batch_size does not do anything
         num_batches = len(y) // batch_size
     elif num_batches is not None:
         if step_size > truncate_len:
@@ -86,6 +107,7 @@ def prepare_inputs(batch_size=10,
 
     return batches_in, batches_out
 
+
 def convert_to_batches(signals, batch_size=10, steps_ahead=1):
     start = 0
     batches_in = []
@@ -99,4 +121,3 @@ def convert_to_batches(signals, batch_size=10, steps_ahead=1):
         start += batch_size
 
     return np.array(batches_in), np.array(batches_out)
-    
